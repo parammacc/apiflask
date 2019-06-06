06410c864d1799e11d158dd7a1b45f1596973b99
Jesus Garcia Rodriguez <j.e.garcia.rodriguez@accenture.com>
2019-06-06 17:28:22 +0200

    eliminar - hago cambios

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def articles():
 		conn.close()
 
 
-@app.route('/article/<cod>', methods=['GET'])
+@app.route('/articles/<cod>', methods=['GET'])
 def article(cod):
 	try:
 		conn = mysql.connect()
@@ -39,14 +39,14 @@ def article(cod):
 		conn.close()
 
 
-@app.route('/article/<cod>', methods=['DELETE'])
-def delete(cod):
+@app.route('/articles/<cod>', methods=['DELETE'])
+def eliminar(cod):
 	try:
 		conn = mysql.connect()
 		cursor = conn.cursor()
 		cursor.execute("DELETE FROM articulos WHERE codigo=%s", (cod,))
 		conn.commit()
-		resp = jsonify('User deleted successfully!')
+		resp = jsonify('Artículo eliminado')
 		resp.status_code = 200
 		return resp
 	except Exception as e:
@@ -54,6 +54,49 @@ def delete(cod):
 	finally:
 		cursor.close() 
 		conn.close()
+
+
+
+@app.route('/articles', methods=['POST'])
+def add_user():
+	try:
+		_json = request.json
+		_name = _json['name']
+		_email = _json['email']
+		_password = _json['pwd']
+		# validate the received values
+		if _name and _email and _password and request.method == 'POST':
+			#do not save password as a plain text
+			_hashed_password = generate_password_hash(_password)
+			# save edits
+			sql = "INSERT INTO tbl_user(user_name, user_email, user_password) VALUES(%s, %s, %s)"
+			data = (_name, _email, _hashed_password,)
+			conn = mysql.connect()
+			cursor = conn.cursor()
+			cursor.execute(sql, data)
+			conn.commit()
+			resp = jsonify('User added successfully!')
+			resp.status_code = 200
+			return resp
+		else:
+			return not_found()
+	except Exception as e:
+		print(e)
+	finally:
+		cursor.close() 
+		conn.close()
+
+
+@app.errorhandler(404)
+def not_found(error=None):
+    message = {
+        'status': 404,
+        'message': 'Not Found: ' + request.url,
+    }
+    resp = jsonify(message)
+    resp.status_code = 404
+
+    return resp
 
 
 if __name__ == "__main__":
