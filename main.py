39c5281627083bc1cf18c0ef0dd8388f12dcae9b
Jesus Garcia Rodriguez <j.e.garcia.rodriguez@accenture.com>
2019-06-06 17:14:05 +0200

    R y D

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from flask import jsonify
 from flask import flash, request
 from werkzeug import generate_password_hash, check_password_hash
 
-@app.route('/articles')
+@app.route('/articles', method=['GET'])
 def articles():
 	try:
 		conn = mysql.connect()
@@ -22,7 +22,7 @@ def articles():
 		conn.close()
 
 
-@app.route('/article/<cod>')
+@app.route('/article/<cod>', methods=['GET'])
 def article(cod):
 	try:
 		conn = mysql.connect()
@@ -30,6 +30,23 @@ def article(cod):
 		cursor.execute("SELECT * FROM articulos WHERE codigo=%s", cod)
 		row = cursor.fetchone()
 		resp = jsonify(row)
+		resp.status_code = 200
+		return resp
+	except Exception as e:
+		print(e)
+	finally:
+		cursor.close() 
+		conn.close()
+
+
+@app.route('/article/<cod>', methods=['DELETE'])
+def delete(cod):
+	try:
+		conn = mysql.connect()
+		cursor = conn.cursor()
+		cursor.execute("DELETE FROM articulos WHERE codigo=%s", (cod,))
+		conn.commit()
+		resp = jsonify('User deleted successfully!')
 		resp.status_code = 200
 		return resp
 	except Exception as e:
